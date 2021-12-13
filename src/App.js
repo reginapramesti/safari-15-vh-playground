293836c639d3971a83e59af75e524affd84eff02
Regina Pramesti <pramesti@canva.com>
2021-12-13 16:31:24 +1100

    Create reduced cases

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import { createBrowserHistory } from 'history';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
+  const history = createBrowserHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/designs">
+          <Designs />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+      </Switch>
+      <ul class="navtabs">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/designs">Designs</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+      </ul>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <h3 class="stickyheader">Body scrolling</h3>
+      <div class="container">
+        <div class="box darkgray"/>
+        <div class="box darkgray"/>
+        <div class="box darkgray"/>
+        <div class="box darkgray"/>
+        <div class="box darkgray"/>
+        <div class="box darkgray"/>
+      </div>
+    </>
+  );
+}
+
+function Designs() {
+  return (
+    <>
+      <h3 class="stickyheader">Div scrolling container height 100vh</h3>
+      <div class="container fixedHeightVh">
+        <div class="box gray"/>
+        <div class="box gray"/>
+        <div class="box gray"/>
+        <div class="box gray"/>
+        <div class="box gray"/>
+        <div class="box gray"/>
+      </div>
+    </>
+  );
+}
+
+function Menu() {
+  return (
+    <>
+      <h3 class="stickyheader">Div scrolling container height 100%</h3>
+      <div class="container fixedHeightPercent">
+        <div class="box lightgray"/>
+        <div class="box lightgray"/>
+        <div class="box lightgray"/>
+        <div class="box lightgray"/>
+        <div class="box lightgray"/>
+        <div class="box lightgray"/>
+      </div>
+    </>
   );
 }
 
